d3675a826fb0b96af7c958c44035a844927d2d90
Eloi Vallverdú <eleloi@gmail.com>
2024-07-24 12:38:07 +0200

    del unused old auto_split_folders script

--- .config/tmux/auto_split_folders.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for directory in ./*/; do
-  if [ -d "$directory" ]; then
-    directory_name=$(basename "$directory")
-    tmux new-window -c "$directory_name" -n "$directory_name" nvim
-  fi
-done
