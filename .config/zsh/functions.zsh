79e085d3124dcfdc8aaa0c2a4596a8cb58ba0563
Eloi Vallverdú <eleloi@gmail.com>
2024-08-18 19:57:07 +0200

    zsh: add project function as p

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -38,3 +38,72 @@ function yy() {
 	fi
 	rm -f -- "$tmp"
 }
+
+# project
+p() {
+    local CYAN='\033[0;36m'
+    local NC='\033[0m' # No Color
+
+    local folders=(
+        "$HOME/dev/mossos/microservices"
+        "$HOME/dev/microservices"
+        "$HOME/Dev/mossos/microservices"
+        "$HOME/Dev/microservices"
+        "$HOME/dev"
+        "$HOME/Dev"
+        "$HOME"
+    )
+
+    for folder in "${folders[@]}"; do
+        if [ -d "$folder" ]; then
+            # Change to the directory and exit the loop if successful
+            cd "$folder" || { echo "Failed to change directory to $folder"; return 1; }
+            echo "Changed directory to $folder"
+            break
+        fi
+    done
+
+    local git_dirs=()
+
+    # Use fd to search for directories and check if they contain a .git subdirectory
+    while IFS= read -r dir; do
+        if [ -d "$dir/.git" ]; then
+            # Add the directory to the array
+            git_dirs+=("$dir")
+        fi
+    done < <(fd -t d .)
+
+    # Use fzf to let the user choose a directory from the list
+    local selected_dir
+    selected_dir=$(printf "%s\n" "${git_dirs[@]}" | fzf --prompt="Select a Git repository directory: ")
+
+    if [ -n "$selected_dir" ]; then
+        cd "$selected_dir" || { echo "Failed to change directory to $selected_dir"; return 1; }
+        echo "Changed directory to $selected_dir"
+        git fetch
+
+        if command -v onefetch > /dev/null; then
+            onefetch
+        else
+            echo "onefetch is not installed"
+        fi
+
+        echo "    ${CYAN}-------------------GIT LOG--------------------${NC}"
+        git lg -10
+
+        echo
+        echo "    ${CYAN}-----------------GIT STATUS-------------------${NC}"
+        git s
+
+        local stash_list
+        stash_list=$(git stash list)
+        if [ -n "$stash_list" ]; then
+            echo
+            echo "    ${CYAN}-----------------GIT STASH-------------------${NC}"
+            echo "$stash_list"
+        fi
+    else
+        echo "No directory selected"
+    fi
+}
+
