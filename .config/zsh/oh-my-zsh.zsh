ca0a6ef57c94b7be2833f3845df67f1d257e460d
Eloi Vallverdú <eleloi@gmail.com>
2025-04-03 17:21:10 +0200

    remove some zsh plugins because now we use carapace

--- .config/zsh/oh-my-zsh.zsh
+++ .config/zsh/oh-my-zsh.zsh
@@ -5,31 +5,18 @@ source $HOME/.config/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh
 
 plugins=(
     fzf
-    fzf-tab
     colored-man-pages
     command-not-found
     docker
     pep8
     pip
-    zsh-autosuggestions
-    zsh-fzf-history-search
     sudo
     zsh-syntax-highlighting
 )
 
-# docker enable stacking autocompletion
-zstyle ':completion:*:*:docker:*' option-stacking yes
-zstyle ':completion:*:*:docker-*:*' option-stacking yes
-
 export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
 source $ZSH/oh-my-zsh.sh
 [ -f $HOME/.fzf.zsh ] && source ~/.fzf.zsh
-
-# fzf
-export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_DEFAULT_OPTS="--layout=reverse --inline-info --height=80%"
-
-enable-fzf-tab
 
 # vi mode
 # source /usr/share/zsh/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
