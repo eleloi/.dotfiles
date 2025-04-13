46f08805fd9a6036995f5658336f6affb867404e
Eloi Vallverdú <eleloi@gmail.com>
2025-04-13 03:55:05 +0200

    zsh: restore fzf-tab plugin

--- .config/zsh/oh-my-zsh.zsh
+++ .config/zsh/oh-my-zsh.zsh
@@ -1,10 +1,11 @@
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
-source $HOME/.config/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh
+# source $HOME/.config/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh
 
 plugins=(
     fzf
+    fzf-tab
     colored-man-pages
     command-not-found
     docker
@@ -18,6 +19,27 @@ plugins=(
 export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
 source $ZSH/oh-my-zsh.sh
 [ -f $HOME/.fzf.zsh ] && source ~/.fzf.zsh
+
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+# set descriptions format to enable group support
+# NOTE: don't use escape sequences (like '%F{red}%d%f') here, fzf-tab will ignore them
+zstyle ':completion:*:descriptions' format '[%d]'
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
+# preview directory's content with eza when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
+# custom fzf flags
+# NOTE: fzf-tab does not follow FZF_DEFAULT_OPTS by default
+zstyle ':fzf-tab:*' fzf-flags --color=fg:1,fg+:2 --bind=tab:accept
+# To make fzf-tab follow FZF_DEFAULT_OPTS.
+# NOTE: This may lead to unexpected behavior since some flags break this plugin. See Aloxaf/fzf-tab#455.
+zstyle ':fzf-tab:*' use-fzf-default-opts yes
+# switch group using `<` and `>`
+zstyle ':fzf-tab:*' switch-group '<' '>'
+zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 
 # vi mode
 # source /usr/share/zsh/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
